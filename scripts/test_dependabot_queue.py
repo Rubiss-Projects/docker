23f139b65cc15b91c1c55c394553a38acf3513d1
Rubiss <rubiss@gmail.com>
2026-09-18 14:53:45 -0400

    Give the deployment queue sole control of Dependabot auto-merge

--- scripts/test_dependabot_queue.py
+++ scripts/test_dependabot_queue.py
@@ -31,8 +31,10 @@ class DependabotQueueTests(unittest.TestCase):
             with patch.object(sys, 'argv', ['selector', str(source)]), \
                  patch.dict(os.environ, {'REPOSITORY': 'owner/repo'}), \
                  patch('subprocess.check_output', side_effect=gh), \
+                 patch('subprocess.run') as mutations, \
                  contextlib.redirect_stdout(output), contextlib.redirect_stderr(io.StringIO()):
                 exec(BLOCKS[1], {})
+            self.mutations = [call.args[0] for call in mutations.call_args_list]
             return output.getvalue().splitlines()
 
     def pr(self, number, state='UNKNOWN', body='Updates `vendor/app` from 1.2.0 to 1.3.0'):
@@ -43,6 +45,22 @@ class DependabotQueueTests(unittest.TestCase):
 
     def test_current_oldest_blocks_next_until_checks_and_merge_finish(self):
         self.assertEqual(self.select([self.pr(301), self.pr(293)], {'293': 0}), [])
+        self.assertEqual(len(self.mutations), 1)
+        self.assertEqual(self.mutations[0][3], '293')
+        self.assertIn('--auto', self.mutations[0])
+
+    def test_later_auto_rebased_pr_loses_merge_eligibility_before_oldest_enabled(self):
+        later = self.pr(301)
+        later['autoMergeRequest'] = {'mergeMethod': 'REBASE'}
+        self.select([later, self.pr(293)], {'293': 2})
+        self.assertEqual(self.mutations[0], ['gh', 'pr', 'merge', '301', '--repo', 'owner/repo', '--disable-auto'])
+        self.assertEqual(self.mutations[1][3], '293')
+        self.assertIn('--match-head-commit', self.mutations[1])
+
+    def test_draft_update_not_selected(self):
+        draft = self.pr(292)
+        draft['isDraft'] = True
+        self.assertEqual(self.select([draft, self.pr(293)], {'293': 1}), ['293'])
 
     def test_major_update_not_automerged(self):
         self.assertEqual(self.select([self.pr(292, body='Updates `vendor/app` from 1.0.0 to 2.0.0'), self.pr(293)], {'293': 1}), ['293'])
